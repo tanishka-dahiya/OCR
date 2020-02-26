5f15e7ab935ad715795fcf7197131bdbddedce95
tanishka-dahiya <55584381+tanishka-dahiya@users.noreply.github.com>
2020-02-26 15:48:26 +0530

    ocr

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const multer = require("multer");
+const app = express();
+const fs = require("fs");
+var Tesseract = require("tesseract.js");
+
+//middlewares
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+const PORT = process.env.PORT | 5000;
+
+var Storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, __dirname + "/images");
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  }
+});
+
+var upload = multer({
+  storage: Storage
+}).single("image");
+//route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      console.log(err);
+      return res.send("Something went wrong");
+    }
+
+    var image = fs.readFileSync(
+      __dirname + "/images/" + req.file.originalname,
+      {
+        encoding: null
+      }
+    );
+    Tesseract.recognize(image).then(function(result) {
+      res.send(result.data.hocr);
+    });
+  });
+});
+
+app.get("/showdata", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`Server running on Port ${PORT}`);
+});
